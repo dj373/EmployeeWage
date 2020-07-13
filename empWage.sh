25a32c20ed5366ed4f902ea09a44b7ec09c7d38c
Dhananjay Sachdeva <dhananjay.sachdeva@gmail.com>
2020-07-13 17:04:49 +0530

    Refactored code to store daily wages in a dictionary

--- empWage.sh
+++ empWage.sh
@@ -26,9 +26,9 @@ function getWorkingHours() {
 }
 
 function calcDailyWage() {
-	local workHrs=$1
-	wage=$(( $workHrs * $EMP_RATE_PER_HR ))
-	echo $wage
+        local workHrs=$1
+        wage=$(( $workHrs * $EMP_RATE_PER_HR ))
+        echo $wage
 }
 
 while [[ $totalEmpHr -lt $MAX_HRS_IN_MONTH &&
@@ -37,7 +37,8 @@ do
         (( totalWorkingDays++ ))
         workingHours="$( getWorkingHours $(( RANDOM%3 )) )"
         totalEmpHrs=$(( $totalEmpHrs+$workingHours ))
-	empDailyWage[$totalWorkingDays]="$( calcDailyWage $workingHours )"
+        empDailyWage["$totalWorkingDays"]="$( calcDailyWage $workingHours )"
 done
 totalSalary=$(( $totalEmpHrs+$EMP_RATE_PER_HR ))
 echo "Daily Wage " ${empDailyWage[@]}
+echo "All Days " ${!empDailyWage[@]}
