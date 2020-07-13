890e8af9101844855c3a8aaf3bde09ae0b9e26ca
Dhananjay Sachdeva <dhananjay.sachdeva@gmail.com>
2020-07-13 16:46:10 +0530

    Refactored code using switch case

--- empWage.sh
+++ empWage.sh
@@ -1,16 +1,18 @@
 #! /bin/bash -x
 isPartTime=1
 isFullTime=2
-randomCheck=$(( RANDOM%3 ))
+empCheck=$(( RANDOM%3 ))
 empRatePerHr=20
-if [ $isFullTime -eq $randomCheck  ]
-then
-        empHrs=8;
-elif [ $isPartTime -eq $randomCheck ]
-then
-        empHrs=4
-else
-        empHrs=0
-fi
+case $empCheck in
+	$isFullTime)
+        	empHrs=8;
+		;;
+	$isPartTime)
+	        empHrs=4
+		;;
+	*)
+        	empHrs=0
+		;;
+esac
 salary=$(( $empRatePerHr * $empHrs ))
 echo "Salary is " $salary
