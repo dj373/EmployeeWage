aa09bd6571884646a147f19b521201d7b66bf31d
Dhananjay Sachdeva <dhananjay.sachdeva@gmail.com>
2020-07-13 17:01:11 +0530

    Added functionality to store daily wages in an array

--- empWage.sh
+++ empWage.sh
@@ -11,7 +11,7 @@ totalEmpHr=0
 totalWorkingDays=0
 
 function getWorkingHours() {
-	case $1 in
+        case $1 in
                 $IS_FULL_TIME)
                         workHrs=8;
                         ;;
@@ -22,7 +22,13 @@ function getWorkingHours() {
                         workHrs=0
                         ;;
         esac
-	echo $workHrs
+        echo $workHrs
+}
+
+function calcDailyWage() {
+	local workHrs=$1
+	wage=$(( $workHrs * $EMP_RATE_PER_HR ))
+	echo $wage
 }
 
 while [[ $totalEmpHr -lt $MAX_HRS_IN_MONTH &&
@@ -31,5 +37,7 @@ do
         (( totalWorkingDays++ ))
         workingHours="$( getWorkingHours $(( RANDOM%3 )) )"
         totalEmpHrs=$(( $totalEmpHrs+$workingHours ))
+	empDailyWage[$totalWorkingDays]="$( calcDailyWage $workingHours )"
 done
 totalSalary=$(( $totalEmpHrs+$EMP_RATE_PER_HR ))
+echo "Daily Wage " ${empDailyWage[@]}
